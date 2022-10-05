03a851819087f0b5c99892093c16e7c729c1b83c
POLETTI Stéphane <polettistephane@msn.com>
2022-10-05 17:55:51 +0200

    component thumb done

---
+++ src/components/Thumb/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+//mapper les cards a l'intérieur
+
+const Thumb = () => {
+  return <card>ICI LES CARTES DE LA GALLERIE</card>
+}
+
+export default Thumb
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,15 @@
 import Appartements from '../../components/Appartements'
 import Banner from '../../components/Banner'
+import Thumb from '../../components/Thumb'
+
 function Home() {
   return (
     <div>
       <Banner text="Chez vous, partout et ailleurs" bg="banner-home" />
+
+      <div className="thumb-gallery">
+        <Thumb />
+      </div>
 
       <div className="accueil">
         <Appartements />
