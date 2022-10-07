31bc0ff1dc995f81a837396fb3d9f2a0e0cfb777
POLETTI Stéphane <polettistephane@msn.com>
2022-10-07 09:57:41 +0200

    add arrow icon

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,3 +1,5 @@
+import arrow from '../../assets/Vector.svg'
+
 const Dropdown = (props) => {
   const titre = props.titre
   const texte = props.texte
@@ -5,6 +7,7 @@ const Dropdown = (props) => {
     <div className="logement-dropdown">
       <div className="logement-dropdown-title-arrow">
         <h3>{titre}</h3>
+        <img src={arrow} alt="arrow" />
       </div>
       <div className="logement-dropdown-texte">
         <p>{texte}</p>
