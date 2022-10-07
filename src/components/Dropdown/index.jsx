c9a51caad4e60712420499cae5ef8c607b89fd2d
POLETTI Stéphane <polettistephane@msn.com>
2022-10-07 13:51:21 +0200

    rotate dropdown done

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react'
 import arrow from '../../assets/Vector.svg'
 
 const Dropdown = (props) => {
   const titre = props.titre
   const texte = props.texte
-  return (
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  return isOpen ? (
     <div className="logement-dropdown">
-      <div className="logement-dropdown-title-arrow">
+      <div
+        className="logement-dropdown-title-arrow"
+        onClick={() => setIsOpen(false)}
+      >
         <h3>{titre}</h3>
-        <img src={arrow} alt="arrow" />
+        <img src={arrow} alt="arrow" className="rotate" />
       </div>
       <div className="logement-dropdown-texte">
         <p>{texte}</p>
+      </div>
+    </div>
+  ) : (
+    <div className="logement-dropdown">
+      <div
+        className="logement-dropdown-title-arrow"
+        onClick={() => setIsOpen(true)}
+      >
+        <h3>{titre}</h3>
+        <img src={arrow} alt="arrow" className="no-rotate" />
       </div>
     </div>
   )
