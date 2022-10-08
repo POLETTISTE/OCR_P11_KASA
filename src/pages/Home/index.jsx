0c26d7493bce095c951a1243498565ab3eab9088
POLETTI Stéphane <polettistephane@msn.com>
2022-10-08 13:42:02 +0200

    reverse rotation css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,42 @@
-import Appartements from '../../components/Appartements'
 import Banner from '../../components/Banner'
 import Thumb from '../../components/Thumb'
+import dataLogement from '../../data/data.json'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+  const [appartements, setAppartements] = useState()
+
+  const getApiData = async () => {
+    await fetch({ dataLogement })
+      .then((res) => res.json())
+      .then((data) => setAppartements(data))
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    getApiData()
+  }, [])
+
   return (
     <div>
-      <Banner text="Chez vous, partout et ailleurs" bg="banner-home" />
-
-      <div className="thumb-gallery">
-        <Thumb text={'texte de la photo'} />
-        <Thumb text={'texte de la photo'} />
-        <Thumb text={'texte de la photo'} />
+      <div>
+        {appartements &&
+          appartements.map((appartement) => {
+            // console.log(appartement)
+            return <div>{appartement.cover}</div>
+          })}
       </div>
 
-      <div className="accueil">
-        <Appartements />
+      <div>
+        <Banner text="Chez vous, partout et ailleurs" bg="banner-home" />
+
+        <div className="thumb-gallery">
+          <Thumb text={'texte de la photo'} />
+          <Thumb text={'texte de la photo'} />
+          <Thumb text={'texte de la photo'} />
+        </div>
+
+        <div className="accueil"></div>
       </div>
     </div>
   )
