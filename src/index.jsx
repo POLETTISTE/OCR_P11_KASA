81064aff5005c7778f8ef1f1557b8fadb93eed1f
POLETTI Stéphane <polettistephane@msn.com>
2022-10-22 09:59:11 +0200

    fix w3c validator alerts / end of project

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import About from './pages/About'
 import Home from './pages/Home'
 import Error from './pages/Error'
 import Logement from './pages/Logement'
-
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
@@ -22,7 +21,6 @@ root.render(
         <Route exact path="/" element={<Home />}></Route>
         <Route path="/About" element={<About />}></Route>
         <Route path="/Logement/:id" element={<Logement />}></Route>
-
         <Route path="*" element={<Error />}></Route>
       </Routes>
       <Footer />
